c68ebe31947814c72ecb1d3253ddb566ccaf4745
Reha <example@gmail.com>
2024-04-08 18:06:07 -0700

    DOM elements

--- js/script.js
+++ js/script.js
@@ -1,32 +1,21 @@
 'use strict';
 
-const btns = document.querySelectorAll('button');
-const overlay = document.querySelector('.overlay');
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
 
-// btn.onclick = function() {
-//     alert("STOP!!!");
-// };
+// console.log(document.querySelector('#current').parentNode.parentNode);
 
-let i = 0;
-const deleteElement = (e) => {
-    console.log(e.currentTarget);
-    console.log(e.type);
-    // i++;
-    // if (i == 1) {
-    //     btn.removeEventListener('click', deleteElement);
-    // }
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+for (let node of document.body.childNodes) {
+
+    if (node.nodeName === "#text") {
+        continue;
+    }
+
+    console.log(node);
 }
-
-btns.forEach(btn => {
-    btn.addEventListener('click', deleteElement, {once:true});
-})
-
-// overlay.addEventListener('click', deleteElement);
-
-const link = document.querySelector('a');
-
-link.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    console.log(e.target);
-});
