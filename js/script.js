c3a4ebd47da12b897da14e59778faa83602b4e3b
Reha <example@gmail.com>
2024-05-06 23:52:44 -0700

    descriptors and useful methods

--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
-const p = document.querySelectorAll('p');
+'use strict';
 
-console.log(p);
+const birthday = Symbol('birthday');
 
-const loadScript = (src) => {
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = false;
-    document.body.append(script);
-}
+const user = {
+    name: "Alex",
+    surname: "Smith",
+    [birthday]: '02/08/1999',
+    showMyPublicData: function() {
+        console.log(`${this.name} ${this.surname}`);
+    }
+};
 
-loadScript('js/test.js');
-loadScript('js/some.js');
+// console.log(Object.getOwnPropertyDescriptor(user, 'name'));
+
+// Object.defineProperty(user, 'birthday', {value: "02/08/1999", writable: true});
+
+// console.log(Object.getOwnPropertyDescriptor(user, 'birthday'));
+
+for (let key in user) console.log(key);
+
+Object.defineProperties(user, {
+    name: {writable: false},
+    surname: {writable: false}
+});
+
+console.log(Object.getOwnPropertyDescriptor(user, birthday));
+
+// user.birthday = '02/02/2000';
+
+// console.log(user);
+
+// enumerable
+// writable
+// configurable
