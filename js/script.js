ef5af9d456e91112202b1a0bbffb434c1cbbf6b7
Reha <example@gmail.com>
2024-05-21 16:50:21 -0700

    setTimeOut, setInterval

--- js/script.js
+++ js/script.js
@@ -1,28 +1,40 @@
-const btns = document.querySelectorAll('button'),
-      wrapper = document.querySelector('.btn-block');
+ const btn = document.querySelector('.btn');
+ let timerId,
+    i = 0;
 
-// console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(1));
-// console.log(btns[0].classList.add('red', 'reha'));
-// console.log(btns[0].classList.remove('blue'));
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
 
+    const id = setInterval(frame, 10);
 
-btns[0].addEventListener('click', () => {
-    // if (btns[1].classList.contains('red')) {
-    //     btns[1].classList.remove('red');
-    // } else {
-    //     btns[1].classList.add('red');
-    // }
-
-    btns[1].classList.toggle('red');
-})
-
-wrapper.addEventListener('click', (event) => {
-    if (event.target && event.target.tagName == 'BUTTON') {
-        console.log('Reha');
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
     }
-});
+}
 
-const newBtn = document.createElement('button');
-newBtn.classList.add('red');
-wrapper.append(newBtn);
+
+
+ btn.addEventListener('click', myAnimation);
+ 
+
+//  function reha() {
+//     if (i == 2) {
+//         clearInterval(timerId);
+//     }
+//     console.log('REHA');
+//     i++;
+//  };
+
+//  let id = setTimeout(function log() {
+//     console.log('Salem');
+//     id = setTimeout(log, 500);
+//  }, 500);
+
+
