9f034f660ef25405be929123078e57d6fc2146ff
Reha <example@gmail.com>
2024-05-22 14:24:51 -0700

    function as classes

--- js/script.js
+++ js/script.js
@@ -1,27 +1,49 @@
 'use strict'
 
-const box = document.querySelector('.box'),
-      btn = document.querySelector('button');
+// function User(name, id) {
+//    this.name = name;
+//    this.id = id;
+//    this.human = true;
+//    this.hello = function() {
+//       console.log(`Hello ${this.name}`);
+//    }
+// }
 
-const width = box.clientWidth;
-const height = box.clientHeight;
+// User.prototype.exit = function() {
+//    console.log(`Пользователь ${this.name} ушел`);
+// }
 
-console.log(width, height);
+// const ivan = new User('Ivan', 28);
+// const alex = new User('Alex', 18);
+
+// ivan.hello();
+// alex.hello();
+
+// ivan.exit();
+// alex.exit();
+
+// console.log(ivan);
+// console.log(alex);
 
 
-btn.addEventListener('click', () => {
-   // box.style.height = box.scrollHeight + 'px';
-   console.log(box.scrollTop);
-})
+class User {
+   constructor(name, id) {
+      this.name = name;
+      this.id = id;
+      this.human = true
+   }
+
+   hello() {
+      console.log(`Hello ${this.name}`);
+   }
+
+   exit() {
+      console.log(`Пользователь ${this.name} ушел`);
+   }
+}
 
 
-console.log(box.getBoundingClientRect().top);
 
-const style = window.getComputedStyle(box);
-
-console.log(style.display);
-
-console.log(document.documentElement.scrollTop);
 
 
 
