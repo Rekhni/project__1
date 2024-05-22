f7e6c8273b170c23275589cbad3c9fb3ca5bbe50
Reha <example@gmail.com>
2024-05-22 15:37:06 -0700

    context this

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 'use strict'
 
+// function showThis(a, b) {
+//    console.log(this);
+//    function sum() {
+//       console.log(this);
+//       return a + b;
+//    }
+//    console.log(sum());
+// }
+
+// showThis(4, 5);
+
+
+// const obj = {
+//    a: 20,
+//    b: 15,
+//    sum: function() {
+//       function shout() {
+//          console.log(this);
+//       }
+//       shout();
+//    }
+// };
+
+// obj.sum();
+
 // function User(name, id) {
 //    this.name = name;
 //    this.id = id;
@@ -9,42 +34,54 @@
 //    }
 // }
 
-// User.prototype.exit = function() {
-//    console.log(`Пользователь ${this.name} ушел`);
+// let ivan = new User('Ivan', 23);
+
+// function sayName(surname) {
+//    console.log(this);
+//    console.log(this.name + ' ' + surname);
 // }
 
-// const ivan = new User('Ivan', 28);
-// const alex = new User('Alex', 18);
+// const user = {
+//    name: 'Reha'
+// };
 
-// ivan.hello();
-// alex.hello();
+// sayName.call(user, 'Kuanysh');
+// sayName.apply(user, ['Kuanysh']);
 
-// ivan.exit();
-// alex.exit();
+// function count(num) {
+//    return this * num;
+// }
 
-// console.log(ivan);
-// console.log(alex);
-
-
-class User {
-   constructor(name, id) {
-      this.name = name;
-      this.id = id;
-      this.human = true
-   }
-
-   hello() {
-      console.log(`Hello ${this.name}`);
-   }
-
-   exit() {
-      console.log(`Пользователь ${this.name} ушел`);
-   }
-}
+// const double = count.bind(2);
+// console.log(double(5));
 
 
 
+// 1) Обычная функция: this = window, но если use strict - undefined
+// 2) Контекст у методов обьекта - сам обьект
+// 3) this в конструкторах и классах - это новый экземпляр обьекта
 
 
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', function(){
+//    this.style.backgroundColor = 'green';
+// });
 
 
+// const obj = {
+//    num: 5,
+//    sayNumber: function() {
+//       const say = () => {
+//          console.log(this.num);
+//       };
+//       say();
+//    }
+// }  
+
+// obj.sayNumber();
+
+
+const double = (a, b) => a * b;
+
+console.log(double(11, 3));
